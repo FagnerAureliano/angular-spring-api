45180e843a6667aa5f48f7c8d349830c58d056d5
fagneraureliano <fagner2aureliano@gmail.com>
2023-07-07 13:58:24 -0300

    :tada: init project

---
+++ src/main/java/com/example/crudspring/controller/CourseController.java
@@ -0,0 +1,24 @@
+package com.example.crudspring.controller;
+
+
+import com.example.crudspring.model.Course;
+import com.example.crudspring.repository.CourseRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+    private final CourseRepository repository;
+
+    @GetMapping
+    public List<Course> list() {
+        return repository.findAll();
+    }
+}
