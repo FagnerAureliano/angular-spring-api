56d074994ed2a8d365dff755324e7c87bbe17d21
fagneraureliano <fagner2aureliano@gmail.com>
2023-07-14 13:14:05 -0300

    :sparkles: impl create course endpoint

--- src/main/java/com/example/crudspring/controller/CourseController.java
+++ src/main/java/com/example/crudspring/controller/CourseController.java
@@ -1,14 +1,19 @@
 package com.example.crudspring.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.crudspring.model.Course;
 import com.example.crudspring.repository.CourseRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/api/courses")
@@ -20,5 +25,11 @@ public class CourseController {
     @GetMapping
     public List<Course> list() {
         return repository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return repository.save(course);
     }
 }
--- src/main/java/com/example/crudspring/model/Course.java
+++ src/main/java/com/example/crudspring/model/Course.java
@@ -1,6 +1,8 @@
 package com.example.crudspring.model;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -10,6 +12,7 @@ public class Course {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("_id")
     private Long id;
 
     @Column(length = 200, nullable = false)
