2dd408e58067aca4cf2f1e4f5dde64865def2894
fagneraureliano <fagner2aureliano@gmail.com>
2023-07-15 17:17:22 -0300

    :sparkles: impl find by id course endpoint

--- src/main/java/com/example/crudspring/controller/CourseController.java
+++ src/main/java/com/example/crudspring/controller/CourseController.java
@@ -3,7 +3,9 @@ package com.example.crudspring.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,4 +34,12 @@ public class CourseController {
     public Course create(@RequestBody Course course) {
         return repository.save(course);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findByID(@PathVariable(name = "id") Long id) {
+        return repository.findById(id)
+                .map(course -> ResponseEntity.ok().body(course))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 }
