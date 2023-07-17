f9dc415bfe31a4ff4c96786957cf5c60d7d3ffc2
fagneraureliano <fagner2aureliano@gmail.com>
2023-07-17 19:05:51 -0300

    :sparkles: impl update course endpoint

--- src/main/java/com/example/crudspring/controller/CourseController.java
+++ src/main/java/com/example/crudspring/controller/CourseController.java
@@ -1,21 +1,19 @@
 package com.example.crudspring.controller;
 
+import com.example.crudspring.model.Course;
+import com.example.crudspring.repository.CourseRepository;
 import java.util.List;
-
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.crudspring.model.Course;
-import com.example.crudspring.repository.CourseRepository;
-
-import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/api/courses")
@@ -37,9 +35,24 @@ public class CourseController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Course> findByID(@PathVariable(name = "id") Long id) {
-        return repository.findById(id)
-                .map(course -> ResponseEntity.ok().body(course))
+        return repository
+                .findById(id)
+                .map(recordFound -> ResponseEntity.ok().body(recordFound))
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(
+            @PathVariable Long id,
+            @RequestBody Course course) {
+        return repository
+                .findById(id)
+                .map(recordFound -> {
+                    recordFound.setName(course.getName());
+                    recordFound.setCategory(course.getCategory());
+                    repository.save(recordFound);
+                    return ResponseEntity.ok().body(recordFound);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
