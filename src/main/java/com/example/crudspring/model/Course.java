8bb9d2641eb1a9c0e3cda075e3f68c7e79be2753
fagneraureliano <fagner2aureliano@gmail.com>
2023-07-19 19:33:07 -0300

    :sparkles: impl soft delete

--- src/main/java/com/example/crudspring/model/Course.java
+++ src/main/java/com/example/crudspring/model/Course.java
@@ -1,5 +1,7 @@
 package com.example.crudspring.model;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -12,6 +14,8 @@ import lombok.Data;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?")
+@Where(clause = "status = 'Ativo'")
 public class Course {
 
     @Id
@@ -30,5 +34,11 @@ public class Course {
     @Pattern(regexp = "Web Development|Mobile Development")
     @Column(length = 25, nullable = false)
     private String category;
+
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Ativo|Inativo")
+    @Column(length = 25, nullable = false)
+    private String status = "Ativo";
 
 }
